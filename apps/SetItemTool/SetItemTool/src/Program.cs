75b4f7640d3bb979aae57f2a05b1bb6f7d701673
kurtekat <142125482+kurtekat@users.noreply.github.com>
2023-10-12 00:15:11 -0400

    update SetItem application

---
+++ apps/SetItemTool/SetItemTool/src/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Parsec;
+using Parsec.Shaiya.SetItem;
+
+namespace SetItemTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose an option");
+            Console.WriteLine("1. Extract");
+            Console.WriteLine("2. Compile");
+            var option = Console.ReadKey().KeyChar;
+
+            if (option == '1')
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please choose a format");
+                Console.WriteLine("1. Client");
+                Console.WriteLine("2. Server");
+                option = Console.ReadKey().KeyChar;
+
+                if (option == '1')
+                {
+                    var data = ParsecReader.FromFile<SetItem>("SetItem.SData");
+                    data.WriteJson("SetItem.json");
+                }
+                else
+                {
+                    var data = ParsecReader.FromFile<SetItemEx>("SetItem.SData");
+                    data.WriteJson("SetItem.json");
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please choose a format");
+                Console.WriteLine("1. Client");
+                Console.WriteLine("2. Server");
+                option = Console.ReadKey().KeyChar;
+
+                if (option == '1')
+                {
+                    Directory.CreateDirectory("output/client");
+                    var data = ParsecReader.FromJsonFile<SetItem>("SetItem.json");
+                    data.WriteEncrypted("output/client/SetItem.SData");
+                }
+                else
+                {
+                    Directory.CreateDirectory("output/server");
+                    var data = ParsecReader.FromJsonFile<SetItemEx>("SetItem.json");
+                    data.WriteDecrypted("output/server/SetItem.SData");
+                }
+            }
+        }
+    }
+}
---
+++ apps/SetItemTool/SetItemTool/src/Synergy.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Parsec.Serialization;
+using Parsec.Shaiya.Core;
+
+namespace SetItemTool
+{
+    internal class Synergy : ISerializable
+    {
+        private readonly int _abilityCount = 12;
+
+        [JsonIgnore]
+        public string Text { get; set; } = string.Empty;
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Reaction { get; set; }
+        public int Luck { get; set; }
+        public int Health { get; set; }
+        public int Mana { get; set; }
+        public int Stamina { get; set; }
+        public int MeleeAttackPower { get; set; }
+        public int RangeAttackPower { get; set; }
+        public int MagicAttackPower { get; set; }
+
+        public void Read(SBinaryReader binaryReader)
+        {
+            Text = binaryReader.ReadString();
+            if (string.IsNullOrEmpty(Text))
+                return;
+
+            var text = Text.Split(',');
+            if (text.Length != _abilityCount)
+                return;
+
+            Strength = int.Parse(text[0]);
+            Dexterity = int.Parse(text[1]);
+            Intelligence = int.Parse(text[2]);
+            Wisdom = int.Parse(text[3]);
+            Reaction = int.Parse(text[4]);
+            Luck = int.Parse(text[5]);
+            Health = int.Parse(text[6]);
+            Mana = int.Parse(text[7]);
+            Stamina = int.Parse(text[8]);
+            MeleeAttackPower = int.Parse(text[9]);
+            RangeAttackPower = int.Parse(text[10]);
+            MagicAttackPower = int.Parse(text[11]);
+        }
+
+        public void Write(SBinaryWriter binaryWriter)
+        {
+            var text = string.Format(
+            "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}",
+            Strength,
+            Dexterity,
+            Intelligence,
+            Wisdom,
+            Reaction,
+            Luck,
+            Health,
+            Mana,
+            Stamina,
+            MeleeAttackPower,
+            RangeAttackPower,
+            MagicAttackPower
+            );
+
+            binaryWriter.Write(text);
+        }
+    }
+}
