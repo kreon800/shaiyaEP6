79e5b9eb5e104dee14c04e329fefc39c75641580
kurtekat <142125482+kurtekat@users.noreply.github.com>
2023-12-12 02:06:59 -0500

    Update packet_gem.cpp

--- server/sdev/src/packet_gem.cpp
+++ server/sdev/src/packet_gem.cpp
@@ -72,7 +72,7 @@ namespace packet_gem
         return false;
     }
 
-    void rune_combine_handler(CUser* user, Packet buffer)
+    void item_rune_combine_handler(CUser* user, Packet buffer)
     {
         if (!user->zone)
             return;
@@ -94,7 +94,13 @@ namespace packet_gem
         ItemRuneCombineOutgoing packet{};
         packet.result = ItemRuneCombineResult::Failure;
 
-        if (rune->count < 2 || rune->itemInfo->effect != CGameData::ItemEffect::ItemCompose)
+        if (rune->itemInfo->effect != CGameData::ItemEffect::ItemCompose)
+        {
+            SConnection::Send(&user->connection, &packet, 3);
+            return;
+        }
+
+        if (rune->count < 2)
         {
             SConnection::Send(&user->connection, &packet, 3);
             return;
@@ -137,34 +143,34 @@ namespace packet_gem
             return;
         }
 
-        if (itemInfo)
+        if (!itemInfo)
+            return;
+
+        packet.result = ItemRuneCombineResult::Success;
+        packet.bag = 1;
+        packet.slot = 0;
+        packet.type = itemInfo->type;
+        packet.typeId = itemInfo->typeId;
+        packet.count = 1;
+
+        while (packet.bag <= user->bagsUnlocked)
         {
-            packet.result = ItemRuneCombineResult::Success;
-            packet.bag = 1;
-            packet.slot = 0;
-            packet.type = itemInfo->type;
-            packet.typeId = itemInfo->typeId;
-            packet.count = 1;
+            packet.slot = find_available_slot(user, packet.bag);
 
-            while (packet.bag <= user->bagsUnlocked)
+            if (packet.slot != -1)
             {
-                packet.slot = find_available_slot(user, packet.bag);
-
-                if (packet.slot != -1)
-                {
-                    if (!CUser::ItemCreate(user, itemInfo, packet.count))
-                        break;
-
-                    CUser::ItemUseNSend(user, runeBag, runeSlot, false);
-                    CUser::ItemUseNSend(user, runeBag, runeSlot, false);
-                    CUser::ItemUseNSend(user, vialBag, vialSlot, false);
-
-                    SConnection::Send(&user->connection, &packet, sizeof(ItemRuneCombineOutgoing));
+                if (!CUser::ItemCreate(user, itemInfo, packet.count))
                     break;
-                }
 
-                ++packet.bag;
+                CUser::ItemUseNSend(user, runeBag, runeSlot, false);
+                CUser::ItemUseNSend(user, runeBag, runeSlot, false);
+                CUser::ItemUseNSend(user, vialBag, vialSlot, false);
+
+                SConnection::Send(&user->connection, &packet, sizeof(ItemRuneCombineOutgoing));
+                break;
             }
+
+            ++packet.bag;
         }
     }
 
@@ -499,8 +505,12 @@ void __declspec(naked) naked_0x479FB4()
         je case_0x80D
         cmp eax,0x830
         je case_0x830
+        cmp eax,0x831
+        je case_0x831
         cmp eax,0x832
         je case_0x832
+        cmp eax,0x833
+        je case_0x833
 
         // original
         add eax,-0x801
@@ -511,17 +521,17 @@ void __declspec(naked) naked_0x479FB4()
 
         push esi // packet
         push edi // user
-        call packet_gem::rune_combine_handler
+        call packet_gem::item_rune_combine_handler
         add esp,0x8
         
         popad
 
         jmp exit_switch
 
-        // chaotic squares
-
         case_0x830:
+        case_0x831:
         case_0x832:
+        case_0x833:
         exit_switch:
         pop edi
         pop esi
